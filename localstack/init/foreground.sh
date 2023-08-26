323d3b189846fe4717952803cd7eae3df7063741
Harsh Mishra <erbeusgriffincasper@gmail.com>
2023-08-26 21:26:26 +0530

    Update foreground.sh

--- localstack/init/foreground.sh
+++ localstack/init/foreground.sh
@@ -1,5 +1,5 @@
 docker pull localstack/localstack:latest
-curl https://github.com/localstack/localstack-cli/releases/download/v2.2.0/localstack-cli-2.2.0-linux-amd64-onefile.tar.gz
+wget https://github.com/localstack/localstack-cli/releases/download/v2.2.0/localstack-cli-2.2.0-linux-amd64-onefile.tar.gz
 sudo tar xvzf localstack-cli-2.2.0-linux-amd64-onefile.tar.gz -C /usr/local/bin
 pip3 install awscli
 pip3 install awscli-local[ver1]
