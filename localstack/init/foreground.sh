4c99288af8961c47ca5c81da936928c83cc4c460
Harsh Mishra <erbeusgriffincasper@gmail.com>
2023-08-27 11:25:04 +0530

    Update foreground.sh

--- localstack/init/foreground.sh
+++ localstack/init/foreground.sh
@@ -4,3 +4,7 @@ sudo tar xvzf localstack-cli-2.2.0-linux-amd64-onefile.tar.gz -C /usr/local/bin
 rm localstack-cli-2.2.0-linux-amd64-onefile.tar.gz
 pip3 install awscli
 pip3 install awscli-local[ver1]
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+apt install nodejs
+npm install --global yarn
+npm install -g aws-cdk-local aws-cdk
