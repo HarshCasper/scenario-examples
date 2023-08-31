5b099a3d907f39672e7e32327c0a95d8f2730501
Harsh Mishra <erbeusgriffincasper@gmail.com>
2023-08-31 16:00:39 +0530

    Update foreground.sh

--- localstack/init/foreground.sh
+++ localstack/init/foreground.sh
@@ -4,6 +4,3 @@ sudo tar xvzf localstack-cli-2.2.0-linux-amd64-onefile.tar.gz -C /usr/local/bin
 rm localstack-cli-2.2.0-linux-amd64-onefile.tar.gz
 pip3 install awscli
 pip3 install awscli-local[ver1]
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install terraform
