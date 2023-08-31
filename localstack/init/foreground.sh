e363c425ff1370dd4832488abe2327d2514e2ebd
Harsh Mishra <erbeusgriffincasper@gmail.com>
2023-08-31 15:36:38 +0530

    Update foreground.sh

--- localstack/init/foreground.sh
+++ localstack/init/foreground.sh
@@ -4,7 +4,6 @@ sudo tar xvzf localstack-cli-2.2.0-linux-amd64-onefile.tar.gz -C /usr/local/bin
 rm localstack-cli-2.2.0-linux-amd64-onefile.tar.gz
 pip3 install awscli
 pip3 install awscli-local[ver1]
-curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-apt install nodejs
-npm install --global yarn
-npm install -g aws-cdk-local aws-cdk
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
